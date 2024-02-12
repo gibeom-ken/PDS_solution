eea64641b722c68cc47fc93fd276059f862f820d
Gibeom Kwon <gibeom.kwon@navercorp.com>
2024-02-13 08:48:46 +0900

    PDS DashBoard

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from trino.dbapi import connect
 from datetime import date
-import pandas as pd
 import datetime
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -177,10 +177,115 @@ if st.button('데이터 조회'):
     df_merge_pd = df_merge[df_merge['솔루션명'] == '상품진단 솔루션']
     df_merge_uq = df_merge[df_merge['솔루션명'] == '유입 Quick 모니터링']
     df_merge_tq = df_merge[df_merge['솔루션명'] == '트렌드 Quick 모니터링']
-    
+
+
+    # API 호출량 데이터 호출
+    query = f"""
+    select a.identifier as "식별번호", a.account_id as "계정", b.acnt_nm as "계정명", b.chnl_no as "채널번호", b.chnl_nm as "채널명", b.chnl_url "채널 URL", a.API_CNT as "호출수"
+    from (
+        select distinct identifier,  account_id, count(identifier) as API_CNT
+        from fdp__db_ec_base.sp_ms_api_center_api_access_log
+        where dt between '{start_date}' AND '{end_date}'
+            and identifier not in ('lZSXhajTiKCCJ2LHtDiY1', '29EKieYFSYNlA6f5VJZ2tl', '6ZelpkkYETOvWuLh6stMj', '6eKwxY9Lp4MpYflxnrobGf')
+            and (gateway_route_url like '%/v1/bizdata-stats%' or gateway_route_url like '%/v1/customer-data%' )
+        group by 1,2
+        ) a
+        
+    left join
+        (
+        select acnt_id, acnt_nm, chnl_no, chnl_nm, chnl_type_nm, chnl_url
+        from bi__db_ec_mart.sp_nvs_seller_hist
+        where dt = '2024-01-24'
+            and chnl_stat_cd = '900'
+            and chnl_type_nm = '스마트스토어'
+        ) b
+        on a.account_id = b.acnt_id	
+    order by a.API_CNT desc
+    """
+
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    cols = [column[0] for column in cur.description]
+    df_api = pd.DataFrame.from_records(data=rows, columns=cols)
+
+
+    # API 요금제 데이터 호출
+    query = f"""
+        select
+            concat('{start_date}','~', '{end_date}') as "분석 기간"
+            , sbc.p_ymd              as "기준일"
+        , cd_solu.comm_cd_nm              as "커머스 솔루션"
+            , sbc.sbc_fees_cd       as "구독 요금제"
+            , count(distinct sbc.acnt_id)     as "판매자 수"
+        
+    from    nimo__db_ec.ns_ec_solu_sbc_md01     sbc
+    left join (
+                select ymd.base_ymd         as base_ymd
+                    , wk.base_strt_ymd     as base_ywk_nm
+                    , concat(ymd.base_y,'-',ymd.base_m) as base_ym
+                    , concat(ymd.base_y, '-', ymd.base_qtr, 'Q') as base_qtr
+                    , ymd.base_y           as base_y
+                from nimo__db_ec.nc_cd_cldr_ymd ymd
+                left join
+                    nimo__db_ec.nc_cd_cldr_wk wk
+                on   ymd.base_ywk = wk.base_ywk
+        ) cal
+    on      sbc.p_ymd = cal.base_ymd
+    left join
+            nimo__db_ec.nc_cd_comm_mtr          cd_stat
+    on      sbc.sbc_stat_cd = cd_stat.comm_cd
+    and     cd_stat.grp_id = 'sbc_stat_cd'
+    left join
+            nimo__db_ec.nc_cd_comm_mtr          cd_solu
+    on      sbc.solu_id = cd_solu.comm_cd
+    and     cd_solu.grp_id = 'solu_id'
+    left join
+            nimo__db_ec.ns_ec_prod_catg_mtr     ctg
+    on      sbc.acnt_rep_prod_catgl_cd = ctg.prod_catg_cd
+    and     ctg.catg_lv = 1
+    left join
+            nimo__db_ec.nc_cd_comm_mtr          cd_acnt_tp
+    on      sbc.rep_acnt_tp_cd = cd_acnt_tp.comm_cd
+    and     cd_acnt_tp.grp_id = 'rep_acnt_tp_cd'
+    left join
+            nimo__db_ec.nc_cd_comm_mtr          cd_acnt_biz
+    on      sbc.rep_acnt_biz_tp_cd = cd_acnt_biz.comm_cd
+    and     cd_acnt_biz.grp_id = 'rep_acnt_biz_tp_cd'
+    left join
+            (select
+                    acnt_id, max(acnt_nm) as acnt_nm
+            from   nimo__db_ec.ns_ec_chnl_info_mtr
+            group by 1
+            ) acnt
+    on      sbc.acnt_id = acnt.acnt_id
+
+    where   1 = 1
+    and     (sbc.p_ymd between '{start_date}' and '{end_date}')
+    and    sbc.solu_id in ('4HiHNUP9KNULMFDijHhZ5f')        
+    and    sbc.sbc_stat_cd in ('SUBSCRIBING','ON_PAYMENT_FAIL','WAITING_UNSUBSCRIPTION','WAITING_SUBSCRIPTION','CANCEL_SUBSCRIPTION','ON_EXAMINATION','UNSUBSCRIBED')     --> required filter : 구독 상태
+    and     case when sbc.bef_end_ymd is not null then 'Y' else 'N' end in ('Y','N')   --> optional filter : 솔루션 재구독 여부
+    group by
+            concat('{start_date}','~', '{end_date}')
+            , sbc.p_ymd
+        , cd_solu.comm_cd_nm              
+            , sbc.sbc_fees_cd       
+    order by
+            concat('{start_date}','~', '{end_date}')
+            , sbc.p_ymd
+        , cd_solu.comm_cd_nm              
+            , sbc.sbc_fees_cd       
+
+    """
+
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    cols = [column[0] for column in cur.description]
+    df_api_cnt = pd.DataFrame.from_records(data=rows, columns=cols)
+    df_api_cnt = df_api_cnt[['기준일', '구독 요금제', '판매자 수']]
 
     
-
 
 
 
@@ -233,12 +338,14 @@ if st.button('데이터 조회'):
         
     
     #API데이터솔루션
+        
     with tab2:
         dash_1 = st.container()
         with dash_1:
-            st.markdown("<h3 style=''text-align: center;''>요약</h3>", unsafe_allow_html=True)
+            st.markdown("<h3 style=''text-align: center;''>Summary</h3>", unsafe_allow_html=True)
             st.write("")
-
+        
+        #API 매출 Card
         dash_2 = st.container()
         with dash_2:
             total_sales = df_merge_api['발생 매출'].sum()
@@ -255,17 +362,45 @@ if st.button('데이터 조회'):
             col2.metric(label="총 매출", value=f"{round(total_sales):,}원", delta = f"{round(total_sales-total_sales2):,}원")            
             col3.metric(label="24년 KPI", value="310,000,000원")            
             col4.metric(label="KPI 달성률", value= str(kpi_goals)+"%", delta = str(round(kpi_goals-kpi_goals2,2))+"%")
+
+        #API 호출량 Card
+        dash_3 = st.container()
+        with dash_3:
+            st.markdown("<h4 style=''text-align: center;''></h4>", unsafe_allow_html=True)
+            identifier_cnt = len(df_api["계정"].unique())
+            api_cnt = df_api['호출수'].sum()
+            api_free_brand = df_api_cnt[df_api_cnt['기준일'] == str(end_date)][df_api_cnt['구독 요금제']=="FREE"]["판매자 수"].sum()
+            api_paid_brand = df_api_cnt[df_api_cnt['기준일'] == str(end_date)][df_api_cnt['구독 요금제']=="PAID"]["판매자 수"].sum()
+
+            yesterday2 = end_date - timedelta(days=1)
+            yst_api_free_brand = df_api_cnt[df_api_cnt['기준일'] == str(yesterday2)][df_api_cnt['구독 요금제']=="FREE"]["판매자 수"].sum()
+            yst_api_paid_brand = df_api_cnt[df_api_cnt['기준일'] == str(yesterday2)][df_api_cnt['구독 요금제']=="PAID"]["판매자 수"].sum()
+
+    
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric(label="호출 브랜드", value=f"{round(identifier_cnt):,}개")            
+            col2.metric(label="API 호출수", value=f"{round(api_cnt):,}")            
+            col3.metric(label="유료 가입자 브랜드", value=f"{round(api_free_brand):,}개", delta = f"{round(api_free_brand-yst_api_free_brand):,}개")            
+            col4.metric(label="무료 가입자 브랜드", value=f"{round(api_paid_brand):,}개", delta = f"{round(api_paid_brand-yst_api_paid_brand):,}개")
+            
         
+        st.markdown("<h3 style=''text-align: center;''> </h3>", unsafe_allow_html=True)
         st.markdown("일별 매출")
         st.bar_chart(data=df_merge_api, x='기준일', y='발생 매출', color=None, width=0, height=0, use_container_width=True)
-
         st.markdown("누적 매출")
         st.bar_chart(data=df_merge_api, x='기준일', y='누적 매출', color=None, width=0, height=0, use_container_width=True)
-    
         st.markdown("KPI 달성률")
         st.line_chart(data=df_merge_api, x='기준일', y='달성률', color=None, width=0, height=0, use_container_width=True)
         
+                
+        st.markdown("<h3 style=''text-align: center;''> </h3>", unsafe_allow_html=True)
+        st.markdown("<h3 style=''text-align: center;''>Detail</h3>", unsafe_allow_html=True)
+        st.markdown("<h4 style=''text-align: center;''>매출 현황 상세</h4>", unsafe_allow_html=True)
         st.dataframe(data=df_merge_api, width=None, height=None, use_container_width=True, hide_index=True, column_order=None, column_config=None)
+        st.markdown("<h4 style=''text-align: center;''>브랜드별 호출량 상세</h4>", unsafe_allow_html=True)
+        st.dataframe(data=df_api, width=None, height=None, use_container_width=True, hide_index=True, column_order=None, column_config=None)
+        st.markdown("<h4 style=''text-align: center;''>요금제별 구독현황 상세</h4>", unsafe_allow_html=True)
+        st.dataframe(data=df_api_cnt, width=None, height=None, use_container_width=True, hide_index=True, column_order=None, column_config=None)
         
     
     
